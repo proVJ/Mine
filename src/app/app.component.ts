5016eca6cbd8a6b6e9a1790b73dbe6e0d8e07416
Vijay Singh <vijaymanral5@gmail.com>
2025-11-15 02:33:21 +0530

    code removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
-import { App } from '@capacitor/app';
 import { AlertBoxService } from './servies/shared/alert-box.service';
 import { AlertBoxComponent } from "./components/shared/alert-box/alert-box.component";
 @Component({
