af32bb4fdff8bfb9ba46ca23037c454ad52234e0
Vijay Singh <vijaymanral5@gmail.com>
2025-11-16 07:08:48 +0530

    fix: update email in inbox and correct title in index file

--- src/app/controls/ButtonControl/ButtonControl.component.ts
+++ src/app/controls/ButtonControl/ButtonControl.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 @Component({
